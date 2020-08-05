5e0e35890ca2504ca485cff582042b8131cb75d6
zeusWesley <eliseu.wp59@gmail.com>
2020-08-05 17:44:48 -0300

    build

---
+++ docs/precache-manifest.1585de2adfd4103bb6a583bdb45c1614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6fd2faffac10ace51fbdf3acdce0ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf5fa8b380c9db66f5e",
+    "url": "/static/css/main.81f205f7.chunk.css"
+  },
+  {
+    "revision": "148be56b09fbf1457870",
+    "url": "/static/js/2.8c5365d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c5365d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf5fa8b380c9db66f5e",
+    "url": "/static/js/main.85b0b832.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
